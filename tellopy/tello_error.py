b61a76ca1e81c27e5d807d67ea3db9596cfd8904
Zahtec <32808999+zahtec@users.noreply.github.com>
2022-06-06 16:19:58 -0700

    Delete tellopy directory

--- tellopy/tello_error.py
+++
@@ -1,7 +0,0 @@
-class TelloError(Exception):
-    '''
-    Internal class for raising errors with Tello as the descriptor. You normally wouldn't use this yourself
-    '''
-
-    def __init__(self, m: str) -> None:
-        super().__init__(m)
